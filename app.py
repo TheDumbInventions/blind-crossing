184cd093d740650d3bd8feca38a61f71b4794850
The Dumb Inventions <thedumbinventions@gmail.com>
2021-06-14 15:46:12 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import torch
 from LYTNetV2 import LYTNetV2
 import numpy as np
 from PIL import Image
-import io
+from io import BytesIO
 from flask import Flask,request,jsonify,render_template
 import os
+import sys
+import base64
 
 model=LYTNetV2()
 model.load_state_dict(torch.load('model.pt', map_location=torch.device('cpu')))
@@ -20,10 +22,22 @@ def render_page():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	file = request.files['file']
-	image_bytes = file.read()
-	im = Image.open(io.BytesIO(image_bytes))
+	#file = request.files['file']
+	#image_bytes = file.read()
+	#im = Image.open(BytesIO(image_bytes))
+	
+	image64 = request.json['image']
+	#image64 = image64.split(';')[1]
+	image64 = image64.split(',')[1]
+	#print(image64, file=sys.stderr)
+	im = Image.open(BytesIO(base64.b64decode(image64))).convert('RGB')
+	
+	#image64 = request.json['image']
+	#im = Image.open(BytesIO(base64.decodebytes(image64.split(',')[1].encode()))).convert('RGB')
+	
+	#im = Image.open(base64.decodebytes(image64.encode('utf-8')))
 	#im=im.rotate(-90, expand=False)
+	#im.show()
 	w, h = 768, 576
 	im = im.resize((w, h))
 	
@@ -38,7 +52,7 @@ def predict():
 	predicted_id = str(predicted.cpu().numpy()[0])
 	points = pred_direc.cpu().detach().numpy()[0].tolist()
 	
-	return jsonify(f"{classes[predicted_id]}  {points}")
+	return jsonify({ "class" : predicted_id, "points" : points })
     
 if __name__ == '__main__':
     app.run(debug=False,port=os.getenv('PORT',5000))
---
+++ static/js/main.js
@@ -0,0 +1,69 @@
+window.onload = function(){ 
+const video = document.getElementById("camera"); //$("#camera");
+const canvas = window.canvas = document.getElementById("videoCanvas"); //$("#result");
+const canvas_ctx = canvas.getContext('2d');
+const result = document.getElementById("result"); //$("#result");
+
+const classes = ['red', 'green', 'countdown_green', 'countdown_blank', 'none'];
+
+canvas.width = 768;
+canvas.height = 576;
+
+video.onplay = function() {
+  setTimeout(updateRequest, 300);
+};
+
+function updateDirection(points) {
+  //canvas.width = video.videoWidth;
+  //canvas.height = video.videoHeight;
+  canvas.width = video.offsetWidth;
+  canvas.height = video.offsetHeight;
+  //canvas_ctx.drawImage(tmpcanvas, 0, 0, canvas.width, canvas.height);
+  canvas_ctx.strokeStyle = "#FF0000";
+  canvas_ctx.lineWidth = 5;
+  canvas_ctx.beginPath();
+  canvas_ctx.moveTo(points[0]*canvas.width, points[1]*canvas.height);
+  canvas_ctx.lineTo(points[2]*canvas.width, points[3]*canvas.height);
+  canvas_ctx.stroke();
+  setTimeout(updateRequest , 50);
+}
+
+function updateRequest() {
+  //event.preventDefault();
+  var tmpcanvas = document.createElement('canvas');
+  tmpcanvas.width = video.videoWidth;
+  tmpcanvas.height = video.videoHeight;
+  tmpcanvas.getContext('2d').drawImage(video, 0, 0, tmpcanvas.width, tmpcanvas.height);
+  var image = tmpcanvas.toDataURL('image/jpeg');
+  $.ajax({
+    type: 'POST',
+    url: '/predict',
+    data: JSON.stringify({"image": image}),
+    dataType: "json",
+    contentType: "application/json",
+    cache: false,
+    processData: false,
+    success: function(data) {
+       updateDirection(data['points']);
+       result.textContent = classes[data['class']];
+       console.log(data['class']);
+    }
+  })
+}
+
+const constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+}
